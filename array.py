640f17143f7dca25657723ff59111b5f849ba8b8
jonnylil12 <www.davidabdo@gmail.com>
2020-09-10 08:50:09 -0400

    deleted part of array.py

--- array.py
+++ array.py
@@ -24,36 +24,10 @@
 #
 #
 #
+
 #
 #
 # x = compact_array('str','d',0)
 #this was changedf in master
-
-from random import randrange
-def numPlayers(cutOffRank,scores):
-    x ,scores = [] , scores.copy()
-    while len(set(x)) != cutOffRank:
-      if scores:
-        x.append(max(scores))
-        scores.remove(max(scores))
-      else:
-        return len(x)
-    return len(x)
-
-
-x = [randrange(10) for _ in range(10)]
-print(numPlayers(4,[20,40,60,60]))
-
-# def calculateWindowMinimums(num, stockPriceDelta, windowSize):
-#     delta = []
-#     for k in range(len(stockPriceDelta)):
-#         temp = []
-#         for a in range(k, windowSize):
-#             temp.append(stockPriceDelta[a])
-#
-#         windowSize += 1
-#         delta.append(min(temp))
-#     return delta
-# print(calculateWindowMinimums(7, [0,4,2,73,11,5,60], 4))
 
 
