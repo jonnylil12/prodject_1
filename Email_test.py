78d92987a59499099ad884a152c205e745d19203
bobdoe945 <bobdoe945@gmail.com>
2020-09-09 21:16:50 -0400

    new file

---
+++ Email_test.py
@@ -0,0 +1,13 @@
+import smtplib
+
+sender = "testingcodingemail1@gmail.com"
+rec = "bobdoe945@gmail.com"
+password = "Password1234$"
+message = "Ur book is due ih 2 days"
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender, password)
+print("Login success")
+server.sendemail(sender, rec, message)
+print("Email has been sent to ", rec)
